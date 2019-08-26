707315f8c42e76b12ea3e1fcd662efdedb641577
MeliseAne <rakhimullinaarina@gmail.com>
2019-08-26 20:35:05 +0300

    Test commit

--- Program.cs
+++ Program.cs
@@ -15,11 +15,11 @@ namespace Shop
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
-
     }
 }
